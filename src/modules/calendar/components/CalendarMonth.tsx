4a12ff156ebfb6b2239c7c2aa1b20a1cf3b7b007
Thiago Rodrigues <thiagonr86@gmail.com>
2021-07-12 21:01:19 -0300

    render days properly

--- src/modules/calendar/components/CalendarMonth.tsx
+++ src/modules/calendar/components/CalendarMonth.tsx
@@ -1,13 +1,43 @@
 import React from 'react';
+import { JsxElement } from 'typescript';
 import '../../../assets/style/main.scss'
 import CalendarDay from './CalendarDay';
 import CalendarHeader from './CalendarHeader';
 
 const CalendarMonth = () => {
+  const month = 6;
+
+  const getLastDateOfMonth = () => {
+    const today = new Date();
+    return new Date(today.getFullYear(), today.getMonth() + 1, 0).getDate();
+  }
+
+  const getFirstDayOfMonth = (year: number, month: number) => {
+    const today = new Date();
+    
+    return new Date(today.getFullYear(), month, 1).getDay();
+  }
+
+  const getCalendarDays = () => {
+    const days = [];
+    const firstDayOfMonth = getFirstDayOfMonth(2021, month);
+    console.log(firstDayOfMonth)
+    for(let i = 0; i < getLastDateOfMonth() - 1; i++) {
+      if( i < firstDayOfMonth) {
+        days.push(<div />)
+      } else {
+        days.push(<CalendarDay />);
+      }
+    }
+    return days;
+  }
+  
   return (
     <div className="calendar-month">
       <CalendarHeader />
-      <CalendarDay />
+      {
+        getCalendarDays()
+      }      
     </div>
   )
 }
