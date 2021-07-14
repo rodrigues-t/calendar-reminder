375e35a5750e4c1e9b22bbc531226fad7c44632d
Thiago Rodrigues <thiagonr86@gmail.com>
2021-07-14 06:57:43 -0300

    add reminder in calendar

--- src/modules/calendar/components/CalendarDay.tsx
+++ src/modules/calendar/components/CalendarDay.tsx
@@ -1,6 +1,8 @@
 import React, { useCallback, useState } from 'react';
 import { Button } from 'react-bootstrap';
 import useCalendarActions from '../hooks/useCalendarActions';
+import useCalendarSelectors from '../hooks/useCalendarSelectors';
+import { Reminder } from '../models/Remider';
 import ReminderFormModal from './modals/ReminderInsertModal';
 
 interface ICalendarDayProps {
@@ -10,6 +12,7 @@ interface ICalendarDayProps {
 const CalendarDay = (props: ICalendarDayProps) => {
   const [showReminderInsertModal, setShowReminderInsertModal] = useState(false);
   const { updateDay } = useCalendarActions();
+  const { reminders, selectedYear, selectedMonth } = useCalendarSelectors();
 
   const addReminderClickCallback = useCallback(
     () => {
@@ -27,22 +30,49 @@ const CalendarDay = (props: ICalendarDayProps) => {
     [setShowReminderInsertModal, updateDay],
   );
 
+  const getDateReminders = () => {
+    return reminders.get(new Date(selectedYear, selectedMonth, props.day).toLocaleDateString('en-US')) ?? [];
+  }
+
+  const getReminderElement = (reminder: Reminder) =>
+    <div>
+      <div className="bullet" style={{ backgroundColor: reminder.color, aspectRatio: "1" }}></div>
+      <span>{reminder.time}</span>
+      <span>{reminder.title}</span>
+    </div>;
+
   return (
     <div className="calendar-month__day" style={{ aspectRatio: "1" }}>
       <div className="calendar-month__day-header">
-        { props.day }
+        {props.day}
       </div>
       <div className="calendar-month__day-body">
-
-      </div>
+        {
+          getDateReminders().slice(0,3).map((reminder: Reminder) => getReminderElement(reminder))
+        }
+        {
+          getDateReminders().length > 0
+          && < section >
+            < Button size="sm" variant="link">
+              {
+                getDateReminders().length > 3
+                ? `${getDateReminders().length - 3}+ manage all`
+                : 'manage reminder(s)'
+              }
+            </Button>
+          </section>
+        }
+      </div >
       <div className="calendar-month__day-footer">
-        <Button size="sm" variant="secondary" block onClick={addReminderClickCallback}>Add Reminder</Button>
+        <Button className="calendar-month__day-button-add" size="sm" variant="secondary" block onClick={addReminderClickCallback}>
+          Add Reminder
+        </Button>
       </div>
       <ReminderFormModal
-        show={showReminderInsertModal} 
+        show={showReminderInsertModal}
         handleClose={handleCloseReminderCallback}
       />
-    </div>
+    </div >
   )
 }
 
